32be0fcdf31a7790f408942187789275a804c433
Your Name <mash.pogonina2014@yandex.ru>
2023-04-16 19:52:55 +0300

    fear: создайть контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
+/* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect('mongodb://127.0.0.1/mestodb', {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('БД подключена'))
+  .catch((err) => console.log(err));
+
+app.use('/users', require('./routes/users'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App listening on port ${PORT}`);
+  console.log('Ссылка на сервер');
+  console.log(BASE_PATH);
 });
